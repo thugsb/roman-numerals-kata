8760d40f19e9662e9ab5a918c672456954963e72
Stuart Basden <mystery@Stuarts-Air.broadband>
2024-02-06 22:50:15 +0000

    Oops, I got it all working before I made any commits! It'll convert numerals<->numbers

--- src/calc.test.ts
+++ src/calc.test.ts
@@ -1,9 +1,42 @@
-import { add } from "../src/calc";
-describe("test add function", () => {
-  it("should return 15 for add(10,5)", () => {
-    expect(add(10, 5)).toBe(15);
+import { convert } from "../src/calc";
+describe("test convert function", () => {
+  it("should throw an error if the input is neither a number nor numeral", () => {
+    expect(() => convert("nan")).toThrow("Please enter a valid number");
   });
-  it("should return 5 for add(2,3)", () => {
-    expect(add(2, 3)).toBe(5);
+  it("should throw an error if the input is more than 3999", () => {
+    expect(() => convert("4000")).toThrow("I can't count above 3999!");
+    expect(() => convert("100000")).toThrow("I can't count above 3999!");
+  });
+  it("should convert single digit numbers correctly", () => {
+    expect(convert("1")).toBe("I");
+    expect(convert("2")).toBe("II");
+    expect(convert("3")).toBe("III");
+    expect(convert("4")).toBe("IV");
+    expect(convert("5")).toBe("V");
+    expect(convert("6")).toBe("VI");
+    expect(convert("7")).toBe("VII");
+    expect(convert("8")).toBe("VIII");
+    expect(convert("9")).toBe("IX");
+  });
+  it("should convert larger numbers correctly", () => {
+    expect(convert("10")).toBe("X");
+    expect(convert("23")).toBe("XXIII");
+    expect(convert("50")).toBe("L");
+    expect(convert("69")).toBe("LXIX");
+    expect(convert("111")).toBe("CXI");
+    expect(convert("500")).toBe("D");
+    expect(convert("999")).toBe("CMXCIX");
+    expect(convert("2020")).toBe("MMXX");
+    expect(convert("3000")).toBe("MMM");
+    expect(convert("3999")).toBe("MMMCMXCIX");
+  });
+  it("should convert roman numerals to arabic numbers", () => {
+    expect(convert("m")).toBe(1000);
+    expect(convert("mm")).toBe(2000);
+    expect(convert("mmm")).toBe(3000);
+    expect(convert("mmcm")).toBe(2900);
+    expect(convert("cm")).toBe(900);
+    expect(convert("MDCCLXIV")).toEqual(1764);
+    expect(convert("i")).toBe(1);
   });
 });
--- src/calc.ts
+++ src/calc.ts
@@ -1,3 +1,125 @@
-export function add(x: number, y: number): number {
-  return x + y;
+export function convert(input: string): string | number {
+  const validNumerals = new RegExp(
+    /^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/i
+  );
+
+  if (!isNaN(parseInt(input))) {
+    // We've been given an arabic number, so we'll convert it to roman numerals
+    const num: number = parseInt(input);
+    if (num >= 4000) {
+      throw new Error("I can't count above 3999!");
+    }
+    return convertNumberToNumeral(num);
+  } else if (validNumerals.test(input)) {
+    // We've been given a valid roman numeral, so we'll convert it to an arabic number
+    const numeralsArray: string[] = input.toUpperCase().split("");
+    const numbersArray = getNumbersArray(numeralsArray);
+    return workOutValue(numbersArray);
+  } else {
+    throw new Error("Please enter a valid number or numeral");
+  }
+}
+
+function getNumbersArray(numeralsArray: string[]) {
+  // I took this algorithm from https://github.com/katie-roberts/romanNumerals/blob/master/script/romannumerals.js
+  // It still needed updating to ES6 and converting to TypeScript tho!
+  const numbersArray: number[] = [];
+  for (var i = 0; i < numeralsArray.length; i++) {
+    switch (numeralsArray[i]) {
+      case "I":
+        numbersArray.push(1);
+        break;
+      case "V":
+        numbersArray.push(5);
+        break;
+      case "X":
+        numbersArray.push(10);
+        break;
+      case "L":
+        numbersArray.push(50);
+        break;
+      case "C":
+        numbersArray.push(100);
+        break;
+      case "D":
+        numbersArray.push(500);
+        break;
+      case "M":
+        numbersArray.push(1000);
+        break;
+    }
+  }
+  return numbersArray;
+}
+
+function workOutValue(numbersArray: number[]) {
+  let total = 0;
+  for (let i = 0; i < numbersArray.length; i++) {
+    if (i < numbersArray.length - 1) {
+      if (numbersArray[i] < numbersArray[i + 1]) {
+        total = total - numbersArray[i];
+      } else {
+        total = numbersArray[i] + total;
+      }
+    } else {
+      total = numbersArray[i] + total;
+    }
+  }
+  return total;
+}
+
+function convertNumberToNumeral(num: number) {
+  let output: string = "";
+  const digits: string[] = String(num).split("");
+  const numDigits = digits.length;
+  for (let i = 0; i < numDigits; i++) {
+    // We'll loop through a number of times equal to the number of digits
+
+    const levels = [
+      ["I", "V", "X"],
+      ["X", "L", "C"],
+      ["C", "D", "M"],
+      ["M", "", ""],
+    ];
+
+    const lastDigit: number = parseInt(digits.pop() as string);
+    // Getting the last digit and working backwards
+
+    let numeralsToPrepend = "";
+    switch (lastDigit) {
+      case 1:
+        numeralsToPrepend += levels[i][0];
+        break;
+      case 2:
+        numeralsToPrepend += levels[i][0] + levels[i][0];
+        break;
+      case 3:
+        numeralsToPrepend += levels[i][0] + levels[i][0] + levels[i][0];
+        break;
+      case 4:
+        numeralsToPrepend += levels[i][0] + levels[i][1];
+        break;
+      case 5:
+        numeralsToPrepend += levels[i][1];
+        break;
+      case 6:
+        numeralsToPrepend += levels[i][1] + levels[i][0];
+        break;
+      case 7:
+        numeralsToPrepend += levels[i][1] + levels[i][0] + levels[i][0];
+        break;
+      case 8:
+        numeralsToPrepend +=
+          levels[i][1] + levels[i][0] + levels[i][0] + levels[i][0];
+        break;
+      case 9:
+        numeralsToPrepend += levels[i][0] + levels[i][2];
+        break;
+
+      default:
+        break;
+    }
+    output = numeralsToPrepend + output;
+  }
+  return output;
 }
